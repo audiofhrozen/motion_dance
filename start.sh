5f04c02941e2d415957ed73e1b38660ac9bdd62c
Nelson Yalta <nyalta21@gmail.com>
2017-12-06 00:53:26 +0900

    deleting

--- start.sh
+++
@@ -1,4 +0,0 @@
-./run_in_docker.sh --gpu 2 --exp single --stage 1
-./run_in_docker.sh --gpu 2 --net lstm_res --exp single --stage 1
-./run_in_docker.sh --gpu 2 --net lstm --exp single --rot quat --stage 1
-./run_in_docker.sh --gpu 2 --net lstm_res --exp single --rot quat --stage 1
