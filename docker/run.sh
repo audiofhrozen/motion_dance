775cffbc8c8d517ebfad9b562629a5d796cfb959
Nelson Yalta <nyalta21@gmail.com>
2018-11-25 22:36:10 +0900

    docker updates

--- docker/run.sh
+++ docker/run.sh
@@ -42,11 +42,15 @@ else
   image_label="dancer:cuda${docker_cuda}-cudnn${docker_cudnn}-ubuntu16.04"
 fi
 
+image_label="${image_label}-user-${HOME##*/}"
 cd ..
 docker_image=$( docker images -q ${image_label} ) 
 if ! [[ -n $docker_image  ]]; then
   echo "Building docker image..."
+  
   build_args="--build-arg FROM_IMAGE=${from_image}"
+  build_args="${build_args} --build-arg THIS_USER=${HOME##*/}"
+  build_args="${build_args} --build-arg THIS_UID=${UID}"
   if [ ! -z "${HTTP_PROXY}" ]; then
     echo "Building with proxy ${HTTP_PROXY}"
     build_args="${build_args} --build-arg WITH_PROXY=${HTTP_PROXY}"
@@ -59,9 +63,8 @@ vol="-v $PWD:/motion_dance " # <=Folder to save data
 
 cmd1="cd /motion_dance"
 cmd2="./run.sh $@"
-cmd3="chmod -R 777 /motion_dance"
 
-cmd="${cmd1}; ${cmd2}; ${cmd3}"
+cmd="${cmd1}; ${cmd2}"
 if [ "${docker_gpu}" == "-1" ]; then
   cmd="docker run -i --rm --name dancer_cpu ${vol} ${image_label} /bin/bash -c '${cmd}'"
 else
